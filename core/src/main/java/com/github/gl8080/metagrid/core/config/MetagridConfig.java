811b5464b7bcaf01c8e5e4a7b890169fe767f07a
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-21 00:20:11 +0900

    設定ファイルをクラスパスから読み込む実装追加

--- core/src/main/java/com/github/gl8080/metagrid/core/config/MetagridConfig.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/config/MetagridConfig.java
@@ -1,5 +1,6 @@
 package com.github.gl8080.metagrid.core.config;
 
+import java.io.InputStream;
 import java.io.Reader;
 
 import javax.xml.bind.JAXB;
@@ -9,7 +10,21 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name="metagrid")
 public class MetagridConfig {
     
-    public static MetagridConfig read(Reader reader) {
+    public static final MetagridConfig instance;
+    
+    static {
+        InputStream resourceAsStream = MetagridConfig.class.getResourceAsStream("/metagrid.xml");
+        if (resourceAsStream == null) {
+            throw new IllegalStateException("metagrid.xml がクラスパス直下に存在しません。");
+        }
+        instance = read(resourceAsStream);
+    }
+    
+    static MetagridConfig read(InputStream input) {
+        return JAXB.unmarshal(input, MetagridConfig.class);
+    }
+    
+    static MetagridConfig read(Reader reader) {
         return JAXB.unmarshal(reader, MetagridConfig.class);
     }
     
