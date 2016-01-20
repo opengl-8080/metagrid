fb31582dc5606508d2058aafdf25007f4ce217bf
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-21 00:20:23 +0900

    動作検証用コード

--- core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
@@ -1,13 +1,24 @@
 package com.github.gl8080.metagrid.core.web;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import com.github.gl8080.metagrid.core.config.MetagridConfig;
 
 @Path("sample")
 public class SampleResource {
     
     @GET
-    public String hello() {
-        return "hello sample";
+    public String hello() throws NamingException {
+        Context ctx = new InitialContext();
+        DataSource ds = (DataSource) ctx.lookup(MetagridConfig.instance.getDatasource().getJndi());
+        
+        System.out.println(ds);
+        
+        return "hello sample!!";
     }
 }
