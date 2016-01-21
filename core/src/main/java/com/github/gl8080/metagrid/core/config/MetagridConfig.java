643b8561cdc261c17a38a588c480e80c4ed9157e
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-22 00:00:04 +0900

    設定ファイルが見つからない場合に分かりやすい例外をスローするように修正

--- core/src/main/java/com/github/gl8080/metagrid/core/config/MetagridConfig.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/config/MetagridConfig.java
@@ -6,19 +6,30 @@ import javax.xml.bind.JAXB;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @XmlRootElement(name="metagrid")
 public class MetagridConfig {
+    private static final Logger logger = LoggerFactory.getLogger(MetagridConfig.class);
     
     private static final String CONFIG_FILE_PATH = "/metagrid.xml";
     
     private static MetagridConfig instance;
     
     public static InputStream getConfigFileStream() {
-        return MetagridConfig.class.getResourceAsStream(CONFIG_FILE_PATH);
+        InputStream in = MetagridConfig.class.getResourceAsStream(CONFIG_FILE_PATH);
+        
+        if (in == null) {
+            throw new IllegalStateException("metagrid.xml がクラスパス直下に存在しません。");
+        }
+        
+        return in;
     }
 
     public static void initialize(InputStream in) {
         instance = JAXB.unmarshal(in, MetagridConfig.class);
+        logger.info("Complete to load configuration file.");
     }
 
     public static MetagridConfig getInstance() {
--- core/src/test/java/com/github/gl8080/metagrid/core/config/MetagridConfigTest.java
+++ core/src/test/java/com/github/gl8080/metagrid/core/config/MetagridConfigTest.java
@@ -5,11 +5,24 @@ import static org.assertj.core.api.Assertions.*;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MetagridConfigTest {
     
+    @Rule
+    public ExpectedException ex = ExpectedException.none();
     
+    @Test
+    public void 設定ファイルがクラスパス直下に存在しない場合は例外をスローする() {
+        // verify
+        ex.expect(IllegalStateException.class);
+        ex.expectMessage("metagrid.xml がクラスパス直下に存在しません。");
+        
+        // exercise
+        MetagridConfig.getConfigFileStream();
+    }
     
     @Test
     public void ルートタグは_metagrid_でマッピングされること() {
