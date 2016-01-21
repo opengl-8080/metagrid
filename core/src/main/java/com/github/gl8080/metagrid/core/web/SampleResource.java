8aba481a512572d664cc4c6e083a730d67ca67bf
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-22 00:26:02 +0900

    Oracle のテーブル一覧を取得してみた

--- core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
@@ -1,8 +1,11 @@
 package com.github.gl8080.metagrid.core.web;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,11 +16,23 @@ import com.github.gl8080.metagrid.core.config.MetagridConfig;
 public class SampleResource {
     
     @GET
-    public String hello() throws NamingException {
+    public String hello() throws Exception {
         Context ctx = new InitialContext();
         DataSource ds = (DataSource) ctx.lookup(MetagridConfig.getInstance().getDatasource().getJndi());
         
-        System.out.println(ds);
+        String sql = "SELECT A.TABLE_NAME, B.COMMENTS FROM USER_TABLES A LEFT JOIN USER_TAB_COMMENTS B ON A.TABLE_NAME=B.TABLE_NAME ORDER BY A.TABLE_NAME";
+        
+        try (Connection con = ds.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery();) {
+            
+            while (rs.next()) {
+                String tableName = rs.getString("TABLE_NAME");
+                String comments = rs.getString("COMMENTS");
+                
+                System.out.println("tableName=" + tableName + ", comments=" + comments);
+            }
+        }
         
         return "hello sample!!";
     }
