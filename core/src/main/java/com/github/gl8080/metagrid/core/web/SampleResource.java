7253f2acbcc8079bb49ccdebf87d81d06c06070c
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-23 01:27:24 +0900

    MySQL からテーブル名一覧をとってみた

--- core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
@@ -16,12 +16,39 @@ import com.github.gl8080.metagrid.core.config.MetagridConfig;
 public class SampleResource {
     
     @GET
-    public String hello() throws Exception {
+    @Path("mysql")
+    public String mysql() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        
+        Context ctx = new InitialContext();
+        DataSource ds = (DataSource) ctx.lookup(MetagridConfig.getInstance().getDatasource().getJndi());
+        
+        String sql = "SELECT TABLE_NAME ,TABLE_COMMENT FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA <> 'information_schema' ORDER BY TABLE_NAME";
+
+        try (Connection con = ds.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery();) {
+            
+            while (rs.next()) {
+                String tableName = rs.getString("TABLE_NAME");
+                String comments = rs.getString("TABLE_COMMENT");
+                
+                sb.append("tableName=").append(tableName).append(", comments=").append(comments).append("\r\n");
+            }
+        }
+        
+        return sb.toString();
+    }
+    
+    @GET
+    @Path("oracle")
+    public String oracle() throws Exception {
         Context ctx = new InitialContext();
         DataSource ds = (DataSource) ctx.lookup(MetagridConfig.getInstance().getDatasource().getJndi());
         
         String sql = "SELECT A.TABLE_NAME, B.COMMENTS FROM USER_TABLES A LEFT JOIN USER_TAB_COMMENTS B ON A.TABLE_NAME=B.TABLE_NAME ORDER BY A.TABLE_NAME";
-        
+
+        StringBuilder sb = new StringBuilder();
         try (Connection con = ds.getConnection();
              PreparedStatement ps = con.prepareStatement(sql);
              ResultSet rs = ps.executeQuery();) {
@@ -29,11 +56,11 @@ public class SampleResource {
             while (rs.next()) {
                 String tableName = rs.getString("TABLE_NAME");
                 String comments = rs.getString("COMMENTS");
-                
-                System.out.println("tableName=" + tableName + ", comments=" + comments);
+
+                sb.append("tableName=").append(tableName).append(", comments=").append(comments).append("\r\n");
             }
         }
         
-        return "hello sample!!";
+        return sb.toString();
     }
 }
