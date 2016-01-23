67c0afbd3e5531aafac319227f1a27eff879970c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-01-24 01:40:27 +0900

    JSON でレスポンスを返すための基本的な仕組みを作成

---
+++ core/src/main/java/com/github/gl8080/metagrid/core/web/convert/JsonWriter.java
@@ -0,0 +1,37 @@
+package com.github.gl8080.metagrid.core.web.convert;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.gl8080.metagrid.core.web.response.MetaGridResponse;
+
+@Provider
+public class JsonWriter implements MessageBodyWriter<MetaGridResponse<?>> {
+    
+    private static final ObjectMapper mapper = new ObjectMapper();
+    
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return mediaType.isCompatible(MediaType.APPLICATION_JSON_TYPE);
+    }
+
+    @Override
+    public long getSize(MetaGridResponse<?> t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(MetaGridResponse<?> value, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        mapper.writeValue(entityStream, value);
+    }
+
+}
---
+++ core/src/main/java/com/github/gl8080/metagrid/core/web/response/MetaGridResponse.java
@@ -0,0 +1,25 @@
+package com.github.gl8080.metagrid.core.web.response;
+
+public class MetaGridResponse<T> {
+    
+    private T value;
+    
+    public static <T> MetaGridResponse<T> of(T value) {
+        MetaGridResponse<T> response = new MetaGridResponse<>();
+        response.value = value;
+        return response;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "MetaGridResponse [value=" + value + "]";
+    }
+}
