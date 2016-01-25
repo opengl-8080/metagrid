f52fc92149293bb5552fd147f24949e2104b6175
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-25 20:03:02 +0900

    データベースの種類を特定できるか検証

--- core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/web/SampleResource.java
@@ -1,8 +1,7 @@
 package com.github.gl8080.metagrid.core.web;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.DatabaseMetaData;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -18,34 +17,22 @@ public class SampleResource {
     @GET
     @Path("mysql")
     public String mysql() throws Exception {
-        String sql = "SELECT TABLE_NAME PHYSICAL_NAME,TABLE_COMMENT LOGICAL_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA <> 'information_schema' ORDER BY TABLE_NAME";
-        return this.getTableNames(sql);
+        return this.getDatabaseInfomation();
     }
     
     @GET
     @Path("oracle")
     public String oracle() throws Exception {
-        String sql = "SELECT A.TABLE_NAME PHYSICAL_NAME, B.COMMENTS LOGICAL_NAME FROM USER_TABLES A LEFT JOIN USER_TAB_COMMENTS B ON A.TABLE_NAME=B.TABLE_NAME ORDER BY A.TABLE_NAME";
-        return this.getTableNames(sql);
+        return this.getDatabaseInfomation();
     }
     
-    private String getTableNames(String sql) throws Exception {
+    private String getDatabaseInfomation() throws Exception {
         Context ctx = new InitialContext();
         DataSource ds = (DataSource) ctx.lookup(MetagridConfig.getInstance().getDatasource().getJndi());
         
-        StringBuilder sb = new StringBuilder();
-        try (Connection con = ds.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql);
-             ResultSet rs = ps.executeQuery();) {
-            
-            while (rs.next()) {
-                String tableName = rs.getString("PHYSICAL_NAME");
-                String comments = rs.getString("LOGICAL_NAME");
-
-                sb.append("tableName=").append(tableName).append(", comments=").append(comments).append("\r\n");
-            }
+        try (Connection con = ds.getConnection();) {
+            DatabaseMetaData metaData = con.getMetaData();
+            return metaData.getDatabaseProductName() + " : " + metaData.getDatabaseProductVersion();
         }
-        
-        return sb.toString();
     }
 }
