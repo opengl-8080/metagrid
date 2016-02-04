28606e796bc1d39ae3643b75bb2183f69b92d0a3
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-05 00:30:56 +0900

    データベース登録処理をリポジトリに移動

---
+++ core/src/main/java/com/github/gl8080/metagrid/core/metadata/additional/AdditionalTableDefinitionRepository.java
@@ -0,0 +1,36 @@
+package com.github.gl8080.metagrid.core.metadata.additional;
+
+import java.util.Objects;
+
+import com.github.gl8080.metagrid.core.MetaGridException;
+import com.github.gl8080.metagrid.core.config.DataSourceConfig;
+import com.github.gl8080.metagrid.core.config.MetagridConfig;
+import com.github.gl8080.metagrid.core.db.JdbcHelper;
+
+public class AdditionalTableDefinitionRepository {
+    
+    public int register(AdditionalTableDefinition def) {
+        Objects.requireNonNull(def);
+        
+        DataSourceConfig repositoryConfig = MetagridConfig.getInstance().getRepositoryDataSource();
+        final JdbcHelper jdbc = new JdbcHelper(repositoryConfig);
+        
+        try {
+            jdbc.beginTransaction();
+            
+            int nextId = jdbc.queryInt("SELECT TABLE_DEFINITION_SEQ.NEXTVAL FROM DUAL");
+            
+            Object[] parameters = {nextId, def.getPhysicalName(), def.getLogicalName()};
+            
+            int cnt = jdbc.update("INSERT INTO TABLE_DEFINITION (ID, PHYSICAL_NAME, LOGICAL_NAME) VALUES (?, ?, ?)", parameters);
+            
+            def.setId(nextId);
+            
+            jdbc.commitTransaction();
+            return cnt;
+        } catch (Exception e) {
+            jdbc.rollbackTransaction();
+            throw new MetaGridException(e);
+        }
+    }
+}
--- core/src/main/java/com/github/gl8080/metagrid/core/web/additional/TableDefinitionResource.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/web/additional/TableDefinitionResource.java
@@ -9,9 +9,8 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import com.github.gl8080.metagrid.core.config.DataSourceConfig;
-import com.github.gl8080.metagrid.core.config.MetagridConfig;
-import com.github.gl8080.metagrid.core.db.JdbcHelper;
+import com.github.gl8080.metagrid.core.metadata.additional.AdditionalTableDefinition;
+import com.github.gl8080.metagrid.core.metadata.additional.AdditionalTableDefinitionRepository;
 import com.github.gl8080.metagrid.core.util.ThrowableConsumer;
 import com.github.gl8080.metagrid.core.web.convert.csv.CsvUploadFile;
 
@@ -23,35 +22,20 @@ public class TableDefinitionResource {
     public Response load(InputStream in) throws IOException {
         CsvUploadFile csv = new CsvUploadFile(in);
         
-        DataSourceConfig repositoryConfig = MetagridConfig.getInstance().getRepositoryDataSource();
-        final JdbcHelper jdbc = new JdbcHelper(repositoryConfig);
-
-        try {
-            csv.each(new ThrowableConsumer<List<String>>() {
+        csv.each(new ThrowableConsumer<List<String>>() {
+            
+            @Override
+            public void consume(List<String> values) throws Exception {
                 
-                @Override
-                public void consume(List<String> value) throws Exception {
-                    jdbc.beginTransaction();
-                    
-                    int nextId = jdbc.queryInt("SELECT TABLE_DEFINITION_SEQ.NEXTVAL FROM DUAL");
-                    
-                    Object[] parameters = new Object[value.size() + 1];
-                    parameters[0] = nextId;
-                    
-                    for (int i=0; i<value.size(); i++) {
-                        parameters[i + 1] = value.get(i);
-                    }
-                    
-                    int cnt = jdbc.update("INSERT INTO TABLE_DEFINITION (ID, PHYSICAL_NAME, LOGICAL_NAME) VALUES (?, ?, ?)", parameters);
-                    System.out.println("cnt = " + cnt);
-                    
-                    jdbc.commitTransaction();
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-            jdbc.rollbackTransaction();
-        }
+                AdditionalTableDefinition def = new AdditionalTableDefinition();
+                def.setPhysicalName(values.get(0));
+                def.setLogicalName(values.get(1));
+                
+                AdditionalTableDefinitionRepository repository = new AdditionalTableDefinitionRepository();
+                repository.register(def);
+                
+            }
+        });
         
         return Response.ok().build();
     }
