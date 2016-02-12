2d90737df4f76c79c9dfd7394a3c271188fddba9
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-12 21:59:56 +0900

    Sql のテスト

--- core/src/main/java/com/github/gl8080/metagrid/core/infrastructure/jdbc/Sql.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/infrastructure/jdbc/Sql.java
@@ -1,12 +1,16 @@
 package com.github.gl8080.metagrid.core.infrastructure.jdbc;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Sql {
     
     private final String text;
     private Object[] parameters = new Object[0];
     
-    public Sql(String string) {
-        this.text = string;
+    public Sql(String text) {
+        Objects.requireNonNull(text);
+        this.text = text;
     }
 
     public String getText() {
@@ -14,7 +18,12 @@ public class Sql {
     }
 
     public void setParameters(Object... parameters) {
+        Objects.requireNonNull(parameters);
         this.parameters = parameters;
+    }
+
+    public void setParameterList(List<Object> parameters) {
+        this.parameters = parameters.toArray();
     }
 
     public Object[] getParameters() {
---
+++ core/src/test/java/com/github/gl8080/metagrid/core/infrastructure/jdbc/SqlTest.java
@@ -0,0 +1,100 @@
+package com.github.gl8080.metagrid.core.infrastructure.jdbc;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class SqlTest {
+
+    @Rule
+    public ExpectedException ex = ExpectedException.none();
+
+    private static final String SQL_TEXT = "select * from test";
+    private Sql sql = new Sql(SQL_TEXT);
+    
+    @Test
+    public void SQLテキストを取得できる() {
+        // exercise
+        String text = sql.getText();
+        
+        // verify
+        assertThat(text).isEqualTo(SQL_TEXT);
+    }
+
+    @Test
+    public void テキストにnullは指定できない() {
+        // verify
+        ex.expect(NullPointerException.class);
+        
+        // exercise
+        new Sql(null);
+    }
+
+    @Test
+    public void パラメータを可変長引数で設定できる() {
+        // setup
+        sql.setParameters(1, "test", true);
+        
+        // exercise
+        Object[] parameters = sql.getParameters();
+        
+        // verify
+        assertThat(parameters).containsExactly(1, "test", true);
+    }
+
+    @Test
+    public void 可変長引数を受け取るメソッドに配列で渡せる() {
+        // setup
+        sql.setParameters(new Object[] {1, "test", true});
+        
+        // exercise
+        Object[] parameters = sql.getParameters();
+        
+        // verify
+        assertThat(parameters).containsExactly(1, "test", true);
+    }
+
+    @Test
+    public void パラメータをListで設定できる() {
+        // setup
+        sql.setParameterList(Arrays.<Object>asList(2, "TEST", false));
+        
+        // exercise
+        Object[] parameters = sql.getParameters();
+        
+        // verify
+        assertThat(parameters).containsExactly(2, "TEST", false);
+    }
+
+    @Test
+    public void パラメータをListで設定するメソッドにnullは渡せない() {
+        // verify
+        ex.expect(NullPointerException.class);
+        
+        // setup
+        sql.setParameterList(null);
+    }
+
+    @Test
+    public void パラメータを配列で設定するメソッドにnullは渡せない() {
+        // verify
+        ex.expect(NullPointerException.class);
+        
+        // setup
+        sql.setParameters((Object[])null);
+    }
+
+    @Test
+    public void 何もパラメータを設定していない場合は_空の配列が返される() {
+        // exercise
+        Object[] parameters = sql.getParameters();
+        
+        // verify
+        assertThat(parameters).isEmpty();
+    }
+    
+}
