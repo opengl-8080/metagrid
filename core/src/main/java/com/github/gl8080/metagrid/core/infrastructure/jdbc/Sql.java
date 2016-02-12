6e19e7b4b3e3607f6cfdac49adf3c884a045f727
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-12 21:32:19 +0900

    整理

--- core/src/main/java/com/github/gl8080/metagrid/core/infrastructure/jdbc/Sql.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/infrastructure/jdbc/Sql.java
@@ -1,29 +1,13 @@
 package com.github.gl8080.metagrid.core.infrastructure.jdbc;
 
 public class Sql {
-//    private static final JdbcHelper jdbc = new JdbcHelper();
     
     private final String text;
-//    private final Supplier<SqlProcessor<R>> processorSupplier;
     private Object[] parameters = new Object[0];
-    
-//    public Sql(String text, Supplier<SqlProcessor<R>> processorSupplier) {
-//        this.text = text;
-//        this.processorSupplier = processorSupplier;
-//    }
     
     public Sql(String string) {
         this.text = string;
-//        this.processorSupplier = null;
     }
-
-//    public R executeQuery() {
-//        SqlProcessor<R> sqlProcessor = this.processorSupplier.get();
-//        
-//        jdbc.query(this.text, sqlProcessor);
-//        
-//        return sqlProcessor.getResult();
-//    }
 
     public String getText() {
         return this.text;
