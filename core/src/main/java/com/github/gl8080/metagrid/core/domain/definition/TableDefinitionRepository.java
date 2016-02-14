2e16bc2e55a0629fa9557f25b6a9674653e4c3a7
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-14 10:02:03 +0900

    サービスローダーから実装を取得

--- core/src/main/java/com/github/gl8080/metagrid/core/domain/definition/TableDefinitionRepository.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/domain/definition/TableDefinitionRepository.java
@@ -6,16 +6,15 @@ import com.github.gl8080.metagrid.core.domain.definition.actual.ActualTableDefin
 import com.github.gl8080.metagrid.core.domain.definition.actual.ActualTableDefinitionRepository;
 import com.github.gl8080.metagrid.core.domain.definition.meta.MetaTableDefinition;
 import com.github.gl8080.metagrid.core.domain.definition.meta.MetaTableDefinitionRepository;
-import com.github.gl8080.metagrid.core.infrastructure.definition.actual.ActualTableDefinitionRepositoryImpl;
-import com.github.gl8080.metagrid.core.infrastructure.definition.meta.MetaTableDefinitionRepositoryImpl;
+import com.github.gl8080.metagrid.core.util.ComponentLoader;
 
 public class TableDefinitionRepository {
     
     public TableDefinitionList findAllTables() {
-        ActualTableDefinitionRepository actualRepo = new ActualTableDefinitionRepositoryImpl();
+        ActualTableDefinitionRepository actualRepo = ComponentLoader.getComponent(ActualTableDefinitionRepository.class);
         List<ActualTableDefinition> actualDefList = actualRepo.findAll();
         
-        MetaTableDefinitionRepository metaRepo = new MetaTableDefinitionRepositoryImpl();
+        MetaTableDefinitionRepository metaRepo = ComponentLoader.getComponent(MetaTableDefinitionRepository.class);
         
         TableDefinitionList list = new TableDefinitionList();
         
