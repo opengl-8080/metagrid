e7f85f6eb7c1259c5361a2c44d99f8af37343c7c
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-23 00:07:24 +0900

    とりあえず CSV アップロード処理をフラットに実装してみている

--- core/src/main/java/com/github/gl8080/metagrid/core/rest/resource/definition/meta/MetaTableDefinitionResource.java
+++ core/src/main/java/com/github/gl8080/metagrid/core/rest/resource/definition/meta/MetaTableDefinitionResource.java
@@ -14,9 +14,23 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import com.github.gl8080.metagrid.core.MetaGridException;
+import com.github.gl8080.metagrid.core.config.MetagridConfig;
+import com.github.gl8080.metagrid.core.domain.definition.actual.ActualTableDefinition;
+import com.github.gl8080.metagrid.core.domain.definition.actual.ActualTableDefinitionRepository;
+import com.github.gl8080.metagrid.core.domain.definition.meta.MetaTableDefinition;
+import com.github.gl8080.metagrid.core.domain.definition.meta.MetaTableDefinitionRepository;
 import com.github.gl8080.metagrid.core.domain.upload.RecordCount;
 import com.github.gl8080.metagrid.core.domain.upload.UploadFile;
 import com.github.gl8080.metagrid.core.domain.upload.UploadFileRepository;
+import com.github.gl8080.metagrid.core.domain.upload.csv.CsvFileUploadTask;
+import com.github.gl8080.metagrid.core.domain.upload.csv.CsvProcessException;
+import com.github.gl8080.metagrid.core.domain.upload.csv.CsvRecordParser;
+import com.github.gl8080.metagrid.core.domain.upload.csv.CsvRecordProcessor;
+import com.github.gl8080.metagrid.core.domain.upload.csv.converter.MetaTableDefinitionCsvConverter;
+import com.github.gl8080.metagrid.core.domain.upload.csv.dto.MetaTableDefinitionCsvRecord;
+import com.github.gl8080.metagrid.core.infrastructure.jdbc.JdbcHelper;
+import com.github.gl8080.metagrid.core.infrastructure.task.AsyncTaskExecutor;
 import com.github.gl8080.metagrid.core.util.ComponentLoader;
 
 @Path("meta-table-definition")
@@ -44,7 +58,40 @@ public class MetaTableDefinitionResource {
                 uploadFile.setRecordCount(recordCount);
             }
             
-            ComponentLoader.getComponent(UploadFileRepository.class).register(uploadFile);
+            JdbcHelper repository = new JdbcHelper(MetagridConfig.getInstance().getRepositoryDataSource());
+            try {
+                repository.beginTransaction();
+                ComponentLoader.getComponent(UploadFileRepository.class).register(uploadFile);
+                repository.commitTransaction();
+            } catch (Exception e) {
+                repository.rollbackTransaction();
+                throw new MetaGridException(e);
+            }
+            
+            final ActualTableDefinitionRepository acutalRepo = ComponentLoader.getComponent(ActualTableDefinitionRepository.class);
+            final MetaTableDefinitionRepository metaRepo = ComponentLoader.getComponent(MetaTableDefinitionRepository.class);
+            
+            CsvRecordProcessor<MetaTableDefinitionCsvRecord> processor = new CsvRecordProcessor<MetaTableDefinitionCsvRecord>() {
+                
+                @Override
+                public void process(MetaTableDefinitionCsvRecord record) {
+                    ActualTableDefinition acutalTableDef = acutalRepo.findByPhysicalName(record.physicalName);
+                    
+                    if (acutalTableDef == null) {
+                        throw new CsvProcessException();
+                    }
+                    
+                    MetaTableDefinition metaTableDef = MetaTableDefinition.from(acutalTableDef);
+                    metaTableDef.setLogicalName(record.logicalName);
+                    
+                    metaRepo.register(metaTableDef);
+                }
+            };
+            MetaTableDefinitionCsvConverter converter = new MetaTableDefinitionCsvConverter(processor);
+            CsvRecordParser parser = new CsvRecordParser(converter);
+            
+            CsvFileUploadTask task = new CsvFileUploadTask(uploadFile, parser);
+            AsyncTaskExecutor.getInstance().dispatch(task);
         } finally {
             tmp.delete();
             System.out.println("delete file");
